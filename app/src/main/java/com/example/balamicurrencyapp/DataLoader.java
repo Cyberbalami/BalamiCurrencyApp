9a64ada305007089f5f44b03f2e55cf913f568d2
Cyberbalami <rajanfreelancer8@gmail.com>
2025-11-25 03:59:05 +0200

    Final commit

--- app/src/main/java/com/example/balamicurrencyapp/DataLoader.java
+++ app/src/main/java/com/example/balamicurrencyapp/DataLoader.java
@@ -1,4 +1,49 @@
 package com.example.balamicurrencyapp;
 
-public class DataLoader {
+import android.content.Context;
+import android.os.AsyncTask;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class DataLoader extends AsyncTask<String, Void, ArrayList<String>> {
+
+    private MainActivity activity;
+
+    public DataLoader(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    protected ArrayList<String> doInBackground(String... urls) {
+        StringBuilder response = new StringBuilder();
+
+        try {
+            URL url = new URL(urls[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+            return Parser.parseJSON(response.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ArrayList<>();
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<String> result) {
+        activity.updateRates(result);
+    }
 }
---
+++ app/src/main/java/com/example/balamicurrencyapp/Parser.java
@@ -0,0 +1,33 @@
+package com.example.balamicurrencyapp;
+
+import org.json.JSONObject;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Parser {
+
+    public static ArrayList<String> parseJSON(String json) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        try {
+            JSONObject root = new JSONObject(json);
+            String base = root.getString("base");
+            JSONObject rates = root.getJSONObject("rates");
+
+            Iterator<String> keys = rates.keys();
+
+            while (keys.hasNext()) {
+                String currency = keys.next();
+                double value = rates.getDouble(currency);
+
+                list.add(currency + " – " + value);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+}
